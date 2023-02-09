e54e880ffb199cbeb9f9be20e034997d58e3bf84
Moulik <msbmoulikraj@gmail.com>
2023-02-09 19:01:20 +0530

    Checking whether given number is strong number or not..

---
+++ Student_Application/Student_Application/IStudent_App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Application
+{
+    interface IStudent_App
+    {
+        void Add_New_Student(Student student);
+        void Get_Student_Details_By_Student_ID(int ID);
+        void Delete_Student_Details_By_Student_ID(int ID);
+        void Update_Student_Details(Student student);
+        void Get_Students_By_Department(string department);
+        void Get_Students_By_Year_Of_Study(int year);
+    }
+}
---
+++ Student_Application/Student_Application/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Application
+{
+    internal class Student
+    {
+        public int Student_ID { get; set; }
+        public string Student_Name { get; set; }
+        public int Father_Name { get; set; }
+        public int Year_Of_Study { get; set; }
+        public string Department { get; set; }
+        public string Contact_Number { get; set; }
+        public string Email_ID { get; set; }
+        public string Father_Contact_Number { get; set; }
+    }
+}
